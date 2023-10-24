e8a9d687b9eb6367c00051addc4cd6360b55e2d7
Nocturnete <gedica@fp.insjoaquimmir.cat>
2023-10-24 19:52:33 +0200

    Correus

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\MailController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
@@ -31,5 +32,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('mail/test', [MailController::class, 'test']);
 
 require __DIR__.'/auth.php';
