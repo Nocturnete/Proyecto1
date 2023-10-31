89ddf2a9230c2d26d856fcb5087299ea04c7ab69
CristianMartinezGuerrero <crmagu@fp.insjoaquimmir.cat>
2023-10-31 20:53:31 +0100

    mejoras

--- resources/views/partials/flash.blade.php
+++ resources/views/partials/flash.blade.php
@@ -1,56 +1,19 @@
+<div id="flash">
 @if ($message = Session::get('success'))
-    <div class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-emerald-500">
-        <span class="text-xl inline-block mr-5 align-middle">
-            <b class="capitalize">CORRECTO:</b>
-        </span>
-        <span class="inline-block align-middle mr-8">
-            <b class="capitalize">{{ $message }}</b>
-        </span>
-    </div>
-@endif 
-
+  @include('partials.flash-message', ['type' => "success", 'message' => $message])
+@endif
 @if ($message = Session::get('error'))
-    <div class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-red-500">
-        <span class="text-xl inline-block mr-5 align-middle">
-            <b class="capitalize">ERROR:</b>
-        </span>
-        <span class="inline-block align-middle mr-8">
-            <b class="capitalize">{{ $message }}</b> 
-        </span>
-    </div>
+  @include('partials.flash-message', ['type' => "danger", 'message' => $message])
 @endif
-
 @if ($message = Session::get('warning'))
-    <div class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-orange-500">
-        <span class="text-xl inline-block mr-5 align-middle">
-            <b class="Capitalize">¡CUIDADO!</b>
-        </span>    
-        <span class="inline-block align-middle mr-8">
-            <b class="capitalize">{{ $message }}</b>
-        </span>
-    </div>
+  @include('partials.flash-message', ['type' => "warning", 'message' => $message])
 @endif
-
 @if ($message = Session::get('info'))
-    <div class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-indigo-500">
-        <span class="text-xl inline-block mr-5 align-middle">
-            <b class="Capitalize">INFO:</b>
-        </span>
-        <span class="inline-block align-middle mr-8">
-            <b class="capitalize">{{ $message }}</b>
-        </span>
-    </div>
+  @include('partials.flash-message', ['type' => "info", 'message' => $message])
 @endif
-
 @if ($errors->any())
-    <div class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-purple-500">
-        <span class="text-xl inline-block mr-5 align-middle">
-            <b class="Capitalize">CUALQUIER:</b>
-        </span>
-        @foreach ($errors->all() as $error)
-            <span class="inline-block align-middle mr-8">
-                <b class="capitalize">{{ $message }}</b>
-            </span>
-        @endforeach
-    </div>
+   @foreach ($errors->all() as $error)
+       @include('partials.flash-message', ['type' => "danger", 'message' => $error])
+   @endforeach
 @endif
+</div>
