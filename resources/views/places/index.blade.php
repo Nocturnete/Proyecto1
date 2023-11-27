91ff2840f3cb17d284c60594a3e3a195eeb60da9
Nocturnete <gedica@fp.insjoaquimmir.cat>
2023-11-27 19:41:12 +0100

    CRUD visibilities

--- resources/views/places/index.blade.php
+++ resources/views/places/index.blade.php
@@ -79,6 +79,7 @@
                             </div>
                         </div>
                     </a>
+                    @endif
                     @endforeach
                     {{ $places->links() }}
                 </div>
