fc0a1e32b74febc40138c8f50989160add483d47
Nocturnete <gedica@fp.insjoaquimmir.cat>
2023-11-28 21:01:11 +0100

    Place Index Ok

--- resources/views/places/create.blade.php
+++ resources/views/places/create.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+    
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Create Place') }}
@@ -76,7 +77,3 @@
         </div>
     </div>
 </x-app-layout>
-
-<script>
-    
-</script>
--- resources/views/places/index.blade.php
+++ resources/views/places/index.blade.php
@@ -1,13 +1,11 @@
 <x-app-layout>
-
     <x-slot name="header">
         <h2 class="font-semibold text-black text-5xl md:text-4xl lg:text-4xl lg:mt-3 dark:text-white">
             {{ __('Lugares') }}
         </h2>
     </x-slot>
-
     <div class="py-5">
-        <div class="w-full mx-auto h-full ">
+        <div class="w-full mx-auto">
             <!-- BUSCADOR -->
             <div class="lg:flex lg:justify-center">
                 <form class="flex items-center mb-8" action="{{ route('places.search') }}" method="GET">
@@ -19,72 +17,61 @@
                     </div>
                 </form>
             </div>
-
             <!-- CREAR LUGAR -->
             <div class="lg:m-6 flex items-center justify-center">
                 <a href="{{ route('places.create') }}">
                     <i class="fi-sr-add text-customblue text-4xl"></i>
                 </a>
             </div>
-
-            <!-- LUGARES -->
-            <div class="w-full mx-auto h-full border-3 border-red-500">
-                <div class="w-full mx-auto h-full flex flex-col">
-                    @foreach ($places as $place)
-                    @if ($place->visibility_id != 3 || (auth()->check() && auth()->user()->id === $place->author_id))
+            <!-- TARGETA -->
+            @foreach ($places as $place)
+                @if ($place->visibility_id != 3 || (auth()->check() && auth()->user()->id === $place->author_id))
                     <a href="{{ route('places.show', $place->id) }}">
-                        <div class="relative flex flex-col mb-3 mt-3 bg-gray-200 space-y-3 rounded-2xl shadow-lg p-3 w-full mx-auto h-full lg:transform lg:transition lg:duration-500 lg:hover:scale-105 lg:hover:shadow-2xl lg:max-w-6xl lg:w-full md:space-y-3 md:space-x-0 md:flex-row dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <div class="rounded-md shadow-md max-w-[900px] flex flex-col md:flex-row mb-8 mt-3 bg-gray-200 dark:bg-gray-500">
                             <!-- IMAGEN -->
-                            <div class="w-full md:w-1/3 grid place-items-center">
-                                <img src="{{ asset('storage/' . $place->file->filepath) }}" class="rounded-xl w-full" />
+                            <div class="w-1/2">
+                                <img src="{{ asset('storage/' . $place->file->filepath) }}" class="object-cover object-center w-full h-full max-w-[500px] max-h-[500px]">
                             </div>
-                            <div class="w-full flex flex-col space-y-4  p-3 lg:w-2/3 lg:h-3/3 ">
-                                <div class="flex justify-between item-center">
-                                    <!-- TITULO -->
-                                    <div class="flex items-center">
-                                        <h3 class="font-black text-gray-800 text-2xl md:text-3xl lg:text-3xl dark:text-white">
+                            <!-- CONTENIDO -->
+                            <div class="w-1/2 w-full">
+                                <div class="h-full">
+                                    <div class="flex justify-between mt-3">
+                                        <!-- TITULO -->
+                                        <h3 class="text-2xl pl-3 pt-3 md:text-3xl lg:text-3xl dark:text-white">
                                             {!! $place->title !!}
                                         </h3>
-                                    </div>
-                                    <!-- FAVORITOS -->
-                                    <div class="flex items-center">
-                                        @can('create', App\Models\Place::class)
-                                            @if(auth()->check() && auth()->user()->favorites->contains($place->id))
-                                            <form action="{{ route('places.unfavorites', ['place' => $place->id]) }}" method="POST" class="flex items-center">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button class="ml-8 pt-2"><i class="fi-sr-star ml-3 text-yellow-500 text-2xl"></i></button>
-                                            </form>
-                                            @else
-                                                <form action="{{ route('places.favorites', ['place' => $place->id]) }}" method="POST" class="flex items-center">
+                                        <!-- FAVORITOS -->
+                                        <div class="flex items-center pr-4">
+                                            @can('create', App\Models\Place::class)
+                                                @if(auth()->check() && auth()->user()->favorites->contains($place->id))
+                                                <form action="{{ route('places.unfavorites', ['place' => $place->id]) }}" method="POST" class="flex items-center">
                                                     @csrf
-                                                    <button class="ml-8 pt-2"><i class="fi-sr-star ml-3 text-2xl"></i></button>
+                                                    @method('DELETE')
+                                                    <button class="ml-8 pt-2"><i class="fi-sr-star ml-3 text-yellow-500 text-2xl"></i></button>
                                                 </form>
-                                            @endif
-                                        @endcan
-                                        <p class="pt-1 text-gray-600 font-bold text-sm ml-2 dark:text-white">
-                                            {{ $place->favorited()->count() }}
-                                        </p>
+                                                @else
+                                                    <form action="{{ route('places.favorites', ['place' => $place->id]) }}" method="POST" class="flex items-center">
+                                                        @csrf
+                                                        <button class="ml-8 pt-2"><i class="fi-sr-star ml-3 text-2xl dark:text-white"></i></button>
+                                                    </form>
+                                                @endif
+                                            @endcan
+                                            <p class="pt-1 text-gray-600 font-bold text-sm ml-2 dark:text-white">
+                                                {{ $place->favorited()->count() }}
+                                            </p>
+                                        </div>
                                     </div>
+                                    <!-- VISIBILIDAD -->
+                                    <p class="ml-3 text-lg dark:text-white">{{ $place->visibility->name }}</p> 
+                                    <!-- DESCRIPCION -->
+                                    <p class="ml-3 mt-4 pr-3 mb-4 text-xl dark:text-white">{!! $place->descripcion !!}</p>
                                 </div>
-                                <!-- DESCRIPCION -->
-                                <p class="text-gray-500 text-lg md:text-lg lg:text-xl dark:text-white">
-                                    {!! $place->description !!}
-                                </p>
-                                <div class="flex space-x-2">
-                                <p class="text-gray-600 dark:text-gray-400">Visibilidad:
-                                    {{ $place->visibility->name }}
-                                </p>
-                            </div>
                             </div>
                         </div>
                     </a>
-                    @endif
-                    @endforeach
-                    {{ $places->links() }}
-                </div>
-            </div>
+                @endif
+            @endforeach
+            {{ $places->links() }}
         </div>
     </div>
-
 </x-app-layout>
