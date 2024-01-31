e66f41c0fa03085e568ece65102d62d190729380
CristianMartinezGuerrero <crmagu@fp.insjoaquimmir.cat>
2024-01-31 18:35:29 +0100

    Autenticació API

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\FileController;
-
+use App\Http\Controllers\Api\TokenController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,9 +15,12 @@ use App\Http\Controllers\Api\FileController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::apiResource('files', FileController::class);
 Route::post('files/{file}', [FileController::class, 'update_workaround']);
+
+Route::post('/login', [TokenController::class, 'login']);
+Route::post('/register', [TokenController::class, 'register']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [TokenController::class, 'user']);
+    Route::post('/logout', [TokenController::class, 'logout']);
+});
