c709349e21b6b26b1adc598f4a49d9e0a793f927
CristianMartinezGuerrero <crmagu@fp.insjoaquimmir.cat>
2024-02-15 18:42:23 +0100

    matomo

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,8 @@ import Alpine from 'alpinejs';
 import './files/create';
 import './places/create';
 import './posts/create';
-import './twe.js'
+import './twe.js';
+import './matomo.js'
 
 window.Alpine = Alpine;
 Alpine.start();
